360883bf1a8d0c920dd8202c0762b8381cf9e8ed
guillermobermejo <90947061+guillermobermejo@users.noreply.github.com>
2024-01-05 21:28:40 -0500

    Update maxProfit.java

--- sliding-window/maxProfit.java
+++ sliding-window/maxProfit.java
@@ -12,7 +12,7 @@ Advantage:  O(1) insertion
 
 Approach: after a stock is bought the max of the remaining range is needed
           calculate this max in an array where the ith value is the max from i to length-1
-          in a loop check to see if the max from a range i (-) new bought price > max
+          in a loop check to see if the max from a range is (-) new bought price > max
 
 Algorithm:
     if check:
@@ -40,6 +40,20 @@ Visualization of max_from[]:
     max_from:   [0,0,0,0,0,0] // initial declaration
     max_from:   [0,0,0,0,0,4] // set last val to last val of prices
     max_from:   [0,6,6,6,6,4] // calculate max from ranges from position[i] and max_from[i+1]
+
+Calculating max:
+    index:       0 1 2 3 4 5
+    prices:     [7,1,5,3,6,4]
+    max_from:   [0,6,6,6,6,4]
+
+    max=0       iteration from: i=0 - i=position.length-2 (i=4)
+
+                                        sell price        buy price
+    when i = 0 -> max = Math.max(max, (max_from[1] (6) - position[0] (7)) (-1)      max=0
+    when i = 1 -> max = Math.max(max, (max_from[2] (6) - position[1] (1)) ( 5)      max=5
+    when i = 2 -> max = Math.max(max, (max_from[3] (6) - position[2] (5)) ( 1)      max=5
+    when i = 3 -> max = Math.max(max, (max_from[4] (6) - position[3] (3)) ( 3)      max=5
+    when i = 4 -> max = Math.max(max, (max_from[5] (4) - position[4] (6)) (-2)      max=5
 */
 
 class Solution {
