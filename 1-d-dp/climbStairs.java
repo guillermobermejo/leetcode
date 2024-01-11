301b0fc0af3bfa747079870fb5779639bbe876f7
guillermobermejo <90947061+guillermobermejo@users.noreply.github.com>
2024-01-11 18:29:42 -0500

    Add files via upload

---
+++ 1-d-dp/climbStairs.java
@@ -0,0 +1,68 @@
+/**
+Programmer: Guillermo               E
+Time Complexity: O(n)
+Space Complexity: O(n)
+Runtime: 0ms (beats 100%)
+Memory: 39.7mb (beats 49.05%)
+
+DS: int[]
+Advantage:  O(1) retrieval
+            O(1) insertion
+            storing previous work done
+
+Approach: 
+    use dynamic programming to store previous work to avoid spending resources on recalculations
+    
+    generate the first few steps by hand - up to 6 steps
+    analyze and come up with a recurrence relation
+    use this recurrence relation to store work in an array
+    
+Analyze:
+    1 step: size:  1    steps: [1]
+    2 step: size:  2    steps: [1+1, 2]
+    3 step: size:  3    steps: [1+1+1, 2+1, 1+2]
+    4 step: size:  5    steps: [1+1+1+1, 2+1+1, 1+2+1, 1+1+2, 2+2]
+    5 step: size:  8    steps: [1+1+1+1+1, 2+1+1+1, 1+2+1+1, 1+1+2+1, 1+1+1+2, 2+2+1, 1+2+1, 1+1+2]
+    6 step: size: 13    steps: [1+1+1+1+1+1, 2+1+1+1+1, 1+2+1+1+1, 1+1+2+1+1, 1+1+1+2+1, 1+1+1+1+2, 2+2+1+1, 2+1+2+1, 2+1+1+2, 1+2+2+1, 1+2+1+2, 1+1+2+2, 2+2+2]
+    
+    recurrence relation: arr[i] = arr[i-1] + arr[1-2]
+    
+    n: 6
+    indexes:         0  1
+    initial values: [1, 2]
+    stored array:   [1, 2, 3, 5, 8, 13]
+
+Algorithm:
+    if check: 
+        if n less than 3, then return n // covers the case when the stairs are only 1 or 2 case
+    
+    delcare & initialize:
+        1 int[] array       name: arr   value: new int[]    size: n
+        arr[0] = 1  // the number of steps possible at 1 step
+        arr[1] = 2  // the number of steps possible at 2 step
+        
+    for loop(int i = 2; i < n; i++):
+        traverse the arr from index 2 to n-1 (end)
+        calculate the number of steps at the ith position from the last two indexes
+        note:
+            arr[i] = arr[i-1] + arr[i-2]
+        
+    return:
+        arr[n-1] // the specified step id the last number calculated
+        
+*/
+
+class Solution {
+    public int climbStairs(int n) {
+        if (n < 3) return n;
+        
+        int[] arr = new int[n];
+        arr[0] = 1;
+        arr[1] = 2;
+        
+        for(int i = 2; i < n; i++)
+            arr[i] = arr[i-1] + arr[i-2];
+    
+        return arr[n-1];
+    }
+}
---
+++ 1-d-dp/minCostClimbingStairs.java
@@ -0,0 +1,77 @@
+/**
+Programmer: Guillermo               E
+Time Complexity: O(n)
+Space Complexity: O(n)
+Runtime: 0ms (beats 100%)
+Memory: 43mb (beats 64.67%)
+
+DS: int[]
+Advantage:  O(1) retrieval
+            O(1) insertion
+            storing previous work done
+
+Approach: 
+    use dynamic programming to store previous work to avoid spending resources on recalculations
+    
+    analyze and come up with a recurrence relation
+    use this recurrence relation to store work in an array
+    
+Analyze:
+    indexes:     0   1   2   3   4   5   6   7   8   9
+    test case: [  1,100,  1,  1,  1,100,  1,  1,100,  1]
+    output:6
+    
+    goal: 
+        store the MINIMUM cost to reach each step after the initial first two
+        useful testcase to visualize recurrence relation: 
+                            [10,15,20]
+            explanation:    here the output is 15 but it is also the MIN of either 
+                                the second to last step 15 
+                                OR
+                                the third to last step plus the last step (10+20=30)
+                                MIN(15, 30) is 15
+    
+    recurrence relation: 
+        arr[i] = cost[i] + MIN(arr[i-1], arr[i-2])
+    
+    indexes:     0   1   2   3   4   5   6   7   8   9
+    cost:      [  1,100,  1,  1,  1,100,  1,  1,100,  1]
+    
+    indexes:     0   1   2   3   4   5   6   7   8   9
+    arr:       [  1,100,  2,  3,  3,103,  4,  5,104,  6]    // min cost of each step array
+    
+    return:
+        the MIN of the last two elements in the new arr min cost 
+        that is either we pay either the second to last step to skip the last and step to FINAL
+        or we are at the last step and pay that price to get to the final step
+
+Algorithm:
+    declare & initialize:
+        1 int[] array       name: arr       value: new int[]    size: nums.length
+        arr[0] = cost[0]
+        arr[1] = cost[1]
+        
+    for loop(int i=2; i<arr.length-1; i++):
+        iterate the cost array from index 2 to arr.length-1 (end)       // covers the case when the array is size two 
+        set the element at the ith position to the min cost to get to this step + its own cost (the recurrence relation)
+        note:
+            arr[i] = cost[i] + Math.min(arr[i-1], arr[i-2])
+            
+    return:
+        the MIN of the last two steps (the min steps needed to get to final step)
+        
+        
+*/
+
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] arr = new int[cost.length];
+        arr[0] = cost[0];
+        arr[1] = cost[1];
+        
+        for (int i = 2; i < cost.length; i++)
+            arr[i] = cost[i] + Math.min(arr[i-1], arr[i-2]);
+           
+        return Math.min(arr[cost.length-1], arr[cost.length-2]);
+    }
+}
