1f6cb1e0b7589637a600627c27c6e11d4758927e
guillermobermejo <90947061+guillermobermejo@users.noreply.github.com>
2024-01-14 18:35:46 -0500

    Update rob.java

--- 1-d-dp/rob.java
+++ 1-d-dp/rob.java
@@ -6,39 +6,40 @@ Runtime: 0ms (beats 100%)
 Memory: 40.8mb (beats 28.98%)
 
 DS: int[]
-Advantage:  O(1) retrieval
-            O(1) insertion
+Advantage:  O(1) Insertion
+            O(1) Access
             storing previous work done
 
 Approach: 
     use dynamic programming to store previous work to avoid spending resources on recalculations
     
-    generate the first few steps by hand - up to 6 steps
+    calculate a few examples by hand to see pattern
     analyze and come up with a recurrence relation
     use this recurrence relation to store work in an array
     
 Analyze:
     nums:   [2,7,9,3,1]
     
-    goal:   obtain the max value for robbing non-adjacent houses
-    avoid:  robbing a house in the 'middle'
-    start:  with the first house worth the most
+    goal:   obtain the max value for robbing non-adjacent houses (ith position == MAX value from house 0 - i inclusive)
+    avoid:  robbing a house that is adjacent to another (immediately left)
+    start:  at the third house (after determining the max profit of the first house alone and the first two houses alone)
     
     sub case: '+' means include
         +
-    [2, 7, 1] -> [2, 7, 7]
+    [2, 7, 1] -> [2, 7, 7]          // not real case i.e., third house changed 9 -> 1
     position:      2 (arr index)
     max profit:    7
         
             the MAX profit from robbing in this neighborhood of 3 houses including all houses is
-            house in middle 7
-     +     +  
-    [2, 7, 6] -> [2, 7, 8]
+            house in middle 7 (7 > other option of robbing two nonadjacent houses 2+1)
+    
+     +     +
+    [2, 7, 9] -> [2, 7, 11]         // real case i.e., first 3 houses
     position:      2 (arr index)
-    max profit:    8
+    max profit:    11
         
             the MAX profit from robbing in this neighborhood of 3 houses including all houses is
-            house in middle on left and right excluding middle 8
+            robbing house on left and right excluding middle with profit being 11 (2+9)
             
     recurence 
     relation: arr[i] = MAX(nums[i] + arr[i-2], arr[i-1])
@@ -64,32 +65,41 @@ Visualization of algorithm:
     start:  arr[0] -> first house: 2    (the max value in a neighborhood of ONE house)      houses: [2]         max: 2
             arr[1] -> second house: 7   (the max value in a neighborhood of TWO houses)     houses: [2, 7]      max: 7
                 note: arr[1] -> MAX(arr[0], arr[1])
+                
+    recurence 
+    relation: arr[i] = MAX(nums[i] + arr[i-2], arr[i-1])
     
     arr @ any ith position is max value of all houses from position 0 up to and including position i
     
     nums:   [2, 7, 9, 3, 1]
     index:   0  1  2  3  4
     arr:    [2, 7, 0, 0, 0]
+    
             i=2 
-            arr[2] = 9 + 2 || 7
+            arr[2] = 9 (nums[2]) + 2 (arr[0]) || 7 (arr[1])
+                                           11 || 7
             = 11                    11 max value from houses 0-2 inclusive                  houses: [2,7,9]     max: 11
     
     index:   0  1  2  3  4
     arr:    [2, 7,11, 0, 0]
+    
             i=3 
-            arr[3] = 3 + 7 || 11
+            arr[3] = 3 (nums[3]) + 7 (arr[1]) || 11 (arr[2])
+                                           10 || 11
             = 11                    11 max value from houses 0-3 inclusive                  houses: [2,7,9,3]   max: 11
             
     index:   0  1  2  3  4
     arr:    [2, 7,11,11, 0]
+    
             i=4 
-            arr[4] = 1 + 11 || 11
+            arr[4] = 1 (nums[4]) + 11 (arr[2]) || 11 (arr[3])
             = 12                    12 max value from houses 0-4 inclusive                  houses: [2,7,9,3,1] max: 12
     
     index:   0  1  2  3  4
     arr:    [2, 7,11,11,12]
+    
+    return arr[nums.length-1] (MAX of houses 0-4 inclusive)
 */
-
 
 class Solution {
     public int rob(int[] nums) {
