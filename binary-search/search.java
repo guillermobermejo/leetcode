b4333cd888d622a765fd75cb4d421986e0d80147
guillermobermejo <90947061+guillermobermejo@users.noreply.github.com>
2024-01-14 01:21:49 -0500

    Update search.java

--- binary-search/search.java
+++ binary-search/search.java
@@ -1,9 +1,9 @@
 /**
-Programmer: Guillermo           *cracked*E
+Programmer: Guillermo       E
 Time Complexity: O(log n)
 Space Complexity: O(1)
 Runtime: 0ms (beats 100%)
-Memory: 45.68mb (beats 19.53%)
+Memory: 45.7 (beats 20.41%)
 
 DS: none
 
@@ -18,25 +18,20 @@ Approach:
     O(log n)
 
 Algorithm:
-    if check:
-        nums length == 1 then return if the sole element is the target
-    
     declare & initialize:
         1 int value     name: l     value: 0                notes: left pointer
-        1 int value     name: r     value: nums.length      notes: right pointer
-        1 int value     name: m     value: -1               notes: middle value (the return)
-    
+        1 int value     name: r     value: nums.length-1    notes: right pointer
+        
     while loop (l < r):
-        if check:
-            if right and left index are beside eachother 
-        
-        calculate:
-            m = ((l+r) / 2) // this is the middle index which we will test
+        declare and initialize:
+            1 int value     name: m     value: (l+r)/2      notes: middle value (the return)
         
         if check:
-            if nums[m] == target we FOUND target, return m
+            if nums[m] == target 
+                true ? we FOUND target, return m
+                false ? continue this iteration of the loop (calculate pointers)
 
-        calculate loop variables:
+        calculate pointers:
             l = target > nums[m] ? m : l  // if target is greater than the middle value, l = m
             r = target < nums[m] ? m : l  // if target is less than the middle value, r = m
 
@@ -51,65 +46,48 @@ Visualization of 1st iterations:
     l = 0              
     r = 5 (length-1)
 
-1st:    
-index:    0 1 2 3 4 5
-range:  |-------------|
-nums:   [-1,0,3,5,9,12]
-    m = 2 (l+r / 2) -> 0+5/2
-    nums[2] = 3
+    1st:      l   m     r
+    index:    0 1 2 3 4 5
+    range:  |-------------|
+    nums:   [-1,0,3,5,9,12]
+        l=0
+        r=5
+        m = 2 (l+r / 2) -> 0+5/2
+        
+        nums[2] = 3
+        3 != target (9)
+        
+        target > r
+        l = m+1
+        l= 3
     
-    nums[3] != target (9)
-    target > r
-    l = m
-    l= 2
+    2nd:            l m r
+    index:    0 1 2 3 4 5
+    range:         |------|
+    nums:   [-1,0,3,5,9,12]
+        l=3
+        r=5
+        m = 4 (l+r / 2) -> 3+5/2   
+        
+        nums[4] = 9
+        9 == target (9)
 
-2nd:         
-index:    0 1 2 3 4 5
-range:       |--------|
-nums:   [-1,0,3,5,9,12]
-    m = 3 (l+r / 2) -> 2+5/2   
-    nums[3] = 5
-    
-    nums[3] != target (9)
-    target > r
-    l = m
-    l = 3
-
-3rd:         
-index:    0 1 2 3 4 5
-range:          |-----|
-nums:   [-1,0,3,5,9,12]
-    m = 4 (l+r / 2) -> 3+5/2
-    nums[4] = 9
-    
-    nums[4] == target (9)
-
-RETURN m
+    RETURN m
 */
 
 class Solution {
     public int search(int[] nums, int target) {
-        if (nums.length == 1) 
-            return nums[0] == target ? 0 : -1;
-
         int l = 0;
         int r = nums.length-1;
-        int m = -1;
 
-        while(l < r) {
-            if (r-l == 1) {
-                if (nums[l] == target) return l;
-                else if (nums[r] == target) return r;
-                return -1;
-            }
-
-            m = ((l+r) / 2);
+        while(l <= r) {
+            int m = (l+r)/2;
 
             if (nums[m] == target)
                 return m;
             
-            l = target > nums[m] ? m : l;
-            r = target < nums[m] ? m : r;
+            l = target > nums[m] ? m+1 : l;
+            r = target < nums[m] ? m-1 : r;
         }
 
         return -1;
