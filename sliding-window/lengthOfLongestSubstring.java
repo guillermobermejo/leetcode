de6f31d09873a40c136a540615282822296e0094
Guillermo Bermejo <guillermo.bermejo@stonybrook.edu>
2024-03-13 16:08:33 -0400

    Update lengthOfLongestSubstring.java

--- sliding-window/lengthOfLongestSubstring.java
+++ sliding-window/lengthOfLongestSubstring.java
@@ -30,6 +30,38 @@ Note:
     ascii values
     '~' - 126   // ending of what we care about
     ' ' - 32    // beninging of what we care about
+    size: '~' - ' ' + 1
+          126 -  32 + 1
+          95
+
+Visualization ascii_map:
+    index        0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 
+    ascii_map: [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ascii for: ' '  !  "  #  $  %  &  '  (  )  *  +  ,  -  .  /  0  1  2  3  4  5  6  7  8  9  :  ;  <  =  >  ? 
+    
+    index:      32 33 34 45 46 47 48 49 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63
+    ascii_map:   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ascii for:   @  A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z  [  \  ]  ^  _ 
+    
+    index:      64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 
+    ascii_map:   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+    ascii for:   `  a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z  {  |  }  ~
+    
+    register ascii structure:
+        ascii_map[char in s - ' ']++
+                  <ascii>   - 32
+                  
+    registering 'a':
+        ascii_map[ a - ' ']++
+                  97 - 32
+        ==
+        ascii_map[65]++
+        
+    registering ' ': (space)
+        ascii_map[' ' - ' ']++
+                  32  - 32
+        ==
+        ascii_map[0]++
 
 Algorithm:
     declare and initialize:
