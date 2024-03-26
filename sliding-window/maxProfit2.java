7709974700a5ebff4987d4fff24ae2a77fccdea1
Guillermo Bermejo <guillermo.bermejo@stonybrook.edu>
2024-03-26 02:02:55 -0400

    comment update

--- sliding-window/maxProfit2.java
+++ sliding-window/maxProfit2.java
@@ -1,10 +1,10 @@
 /**
-Programmer: Guillermo           E       *cracked*
+Programmer: Guillermo           E       *cracked*       (Array Approach)
 Language: Java
 Time Complexity: O(n)
 Space Complexity: O(n)
 Runtime: 2ms (beats 78.72%)
-Memory: 56.94 (beats 97.49%)
+Memory: 56.94mb (beats 97.49%)
 
 DS: int[]
 Advantage:  O(1) insertion
@@ -21,15 +21,15 @@ Algorithm:
         prices.length is 1 ? return 0;
     
     declare:
-        1 int value     name: max
-        1 int[] array   name: max_from      size: position.length
+        1 int value     name: max                                   value: 0
+        1 int[] array   name: max_from      size: prices.length     value: defaults to 0
     
-    initialize:
-        max_from[length-1] = position[length-1] // set last index of max_from to last index of prices for loop
+    set initial value:
+        max_from[length-1] = prices[length-1] // set last index of max_from to last index of prices for loop
 
     for loop:
         iterate max_from from index prices.length-2 to 1 // 1 for reason of being the first max_from after 1st bought
-        set max_from[i] to the max of the current ith position value and the current max_from[i+1]
+        set max_from[i] to the max of the current ith prices value and the current max_from[i+1]
     
     for loop:
         iterate prices from index 0 to prices.length-1
@@ -41,21 +41,21 @@ Visualization of max_from[]:
     prices:     [7,1,5,3,6,4]
     max_from:   [0,0,0,0,0,0] // initial declaration
     max_from:   [0,0,0,0,0,4] // set last val to last val of prices
-    max_from:   [0,6,6,6,6,4] // calculate max from ranges from position[i] and max_from[i+1]
+    max_from:   [0,6,6,6,6,4] // calculate max from ranges from prices[i] and max_from[i+1]
 
 Calculating max:
     index:       0 1 2 3 4 5
     prices:     [7,1,5,3,6,4]
     max_from:   [0,6,6,6,6,4]
 
-    max=0       iteration from: i=0 - i=position.length-2 (i=4)
+    max=0       iteration from: i=0 - i=prices.length-2 (i=4)
 
                                         sell price        buy price
-    when i = 0 -> max = Math.max(max, (max_from[1] (6) - position[0] (7)) (-1)      max=0
-    when i = 1 -> max = Math.max(max, (max_from[2] (6) - position[1] (1)) ( 5)      max=5
-    when i = 2 -> max = Math.max(max, (max_from[3] (6) - position[2] (5)) ( 1)      max=5
-    when i = 3 -> max = Math.max(max, (max_from[4] (6) - position[3] (3)) ( 3)      max=5
-    when i = 4 -> max = Math.max(max, (max_from[5] (4) - position[4] (6)) (-2)      max=5
+    when i = 0 -> max = Math.max(max, (max_from[1] (6) - prices[0] (7)) (-1)        max=0
+    when i = 1 -> max = Math.max(max, (max_from[2] (6) - prices[1] (1)) ( 5)        max=5
+    when i = 2 -> max = Math.max(max, (max_from[3] (6) - prices[2] (5)) ( 1)        max=5
+    when i = 3 -> max = Math.max(max, (max_from[4] (6) - prices[3] (3)) ( 3)        max=5
+    when i = 4 -> max = Math.max(max, (max_from[5] (4) - prices[4] (6)) (-2)        max=5
 */
 
 class Solution {
