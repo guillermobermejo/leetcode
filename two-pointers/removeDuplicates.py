d32638e094a7a68baf435b105f1157ef5e28c748
Guillermo Bermejo <guillermo.bermejo@stonybrook.edu>
2025-09-02 22:53:13 -0400

    Create removeDuplicates.py

---
+++ two-pointers/removeDuplicates.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        slow = 0
+        fast = 0
+
+        # delete logically
+        while fast != len(nums):
+            if nums[fast] == nums[slow]:
+                fast += 1
+            else:
+                slow += 1
+                nums[slow] = nums[fast]
+
+        return slow + 1
